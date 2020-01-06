68b9b515de047af308a612109b0a6a791ebb039e
Abhishek Ranjan <abhishekr700@gmail.com>
2020-01-06 14:14:18 +0530

    Add goto attendance
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- autologin-chrome-extension/contentscript.js
+++ autologin-chrome-extension/contentscript.js
@@ -3,13 +3,52 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const gotoAttendance = async () => {
+    console.log("Click My Activites");
+    let frame = document.getElementById("banner").contentWindow.document;
+    frame.getElementsByClassName("tm-bg")[1].getElementsByTagName("a")[0].click()
+
+    console.log("Wait 5 sec")
+    await sleep(5000)
+
+    let sidebar = document.getElementById("top").contentWindow.document
+    console.log(sidebar);
+    
+    console.log("Click My Attendance Report");
+    sidebar.getElementById("tree").childNodes[6].childNodes[0].childNodes[2].childNodes[0].childNodes[0].click()
+
+    console.log("Wait 5 sec")
+    await sleep(5000)
+
+    let attendanceWindow = document.getElementById("data").contentWindow.document
+    attendanceWindow.getElementById("year").selectedIndex = 2
+    attendanceWindow.getElementById("sem").selectedIndex = 7
+    attendanceWindow.getElementsByTagName("input")[7].click()
+
+}
+
 window.onload = async () => {
+
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+
+            gotoAttendance()
+            if (request.greeting == "hello")
+                sendResponse({ farewell: "goodbye" });
+        });
+
     sleep(4000)
     console.log("After load");
     let c = document.getElementById('banner').contentWindow.document.getElementById('captchaimg');
     // document.getElementById("banner")
     console.log(c)
-    if(c === null) return;
+    if(c === null) {
+        console.log("No Captcha found");
+        return;
+    }
     console.log(c.src);
     
     // let data = await fetch("http://localhost:5000/solveCaptcha", {
@@ -40,10 +79,14 @@ window.onload = async () => {
         console.log(btn);
         
         btn.click()
+        sleep(5000)
     })
 
     // let jsondata = await data.json();
     
     // console.log("data", data)
 
+    
+
 }
+
--- autologin-chrome-extension/popup.js
+++ autologin-chrome-extension/popup.js
@@ -1,9 +1,39 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-changeColor.onclick = function (element) {
-    console.log("Popup click")
-    chrome.tabs.getCurrent(function (tab) {
-        // alert(tab.title);
-        console.log(tab)
-    });
-};
+// changeColor.onclick = function (element) {
+//     console.log("Popup click")
+//     chrome.tabs.getCurrent(function (tab) {
+//         // alert(tab.title);
+//         console.log(tab)
+//     });
+// };
+
+window.onload = () => {
+    let attendance = document.getElementById("attendance")
+    console.log(attendance);
+    attendance.onclick = () => {
+        console.log("Attendance Click");
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
+                console.log(response.farewell);
+            });
+        });
+
+        // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        //     chrome.tabs.executeScript(
+        //         tabs[0].id,
+        //         { code: `
+        //         console.log("Hello")
+        //         console.log("Hello2")
+        //         let frame = document.getElementById("banner").contentWindow.document;
+        //         frame.getElementsByClassName("tm-bg")[1].getElementsByTagName("a")[0].click()
+        //         let sidebar = document.getElementById("top").contentWindow.document
+        //         sidebar.getElementById("tree").childNodes[6].childNodes[0].childNodes[2].childNodes[0].childNodes[0].click()
+
+
+        //         frame = sidebar = null
+        //         ` });
+        // });
+    }
+}
