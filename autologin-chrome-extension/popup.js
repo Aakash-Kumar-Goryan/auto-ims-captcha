884201f916566d66565bf83126a5254b15e50385
Aakash-Kumar-Goryan <aakashgoryan@gmail.com>
2020-01-16 16:59:03 +0530

    AbsentList Added

--- autologin-chrome-extension/popup.js
+++ autologin-chrome-extension/popup.js
@@ -7,31 +7,45 @@
 //         console.log(tab)
 //     });
 // };
-
+let changeClass = (id) => {
+    console.log(id);
+    let element = document.getElementById(id);
+    element.classList.toggle("active");
+}
 window.onload = () => {
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.log(request);
-        let th = "";
-        for(let i=0;i<request.subjectCode.length;i++)
+        let menuContent = `<a id = ${request.subjectCode[0]} class="active item">` + request.subjectCode[0] + `</a>`;
+        for(let i=1;i<request.subjectCode.length;i++)
         {
-            th += "<th>" + request.subjectCode[i] + "</th>";
+            menuContent += `<a id = ${request.subjectCode[i]} class="item">` + request.subjectCode[i] + `</a>`;
         }
-        table = `<thead>
-                    <tr>
-                        ${th}
-                    </tr>
-                </thead>`;
-        console.log(table);
-        document.getElementById("table").innerHTML = table;
+        document.getElementById("subMenu").innerHTML = menuContent;
+        document.getElementById("subMenuValue").innerHTML = request.AbsentList[0];
+        let buttonList = document.getElementById("subMenu").getElementsByTagName('a');
+        for(let i=0;i<buttonList.length;i++)
+        {
+            buttonList[i].onclick = function() {
+                let tempList = document.getElementById("subMenu").getElementsByTagName('a');
+                for(let j=0;j<tempList.length;j++)
+                {
+                    if (tempList[j].classList.contains("active")) {
+                        tempList[j].classList.remove("active");
+                    }
+                }
+                this.classList.add("active");
+                document.getElementById("subMenuValue").innerHTML = request.AbsentList[request.subjectCode.indexOf(this.id)];
+            }
+        }
     });
+
     let attendance = document.getElementById("attendance")
     let login = document.getElementById("login")
-
     console.log(attendance);
+    let subjectLink = null;
     attendance.onclick = () => {
         console.log("Attendance Click");
-
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { msg: 0 }, function (response) {
                 console.log(response.farewell);
