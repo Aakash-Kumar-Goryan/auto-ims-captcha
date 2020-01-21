161d805eab2f34824bc431dae612d1ebe19eea82
Aakash-Kumar-Goryan <aakashgoryan@gmail.com>
2020-01-22 01:29:49 +0530

    UI changes

--- autologin-chrome-extension/popup.js
+++ autologin-chrome-extension/popup.js
@@ -8,29 +8,8 @@ window.onload = () => {
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.log(request);
-        let menuContent = `<a id = ${request.subjectCode[0]} class="active item" data-content="${request.subjectCode[0]}">` + SubjectData[request.subjectCode[0]] + `<div class="ui red label">${request.AbsentList[0].length}</div></a>`;
-        for(let i=1;i<request.subjectCode.length;i++)
-        {
-            menuContent += `<a id = ${request.subjectCode[i]} class="item" data-content="${request.subjectCode[i]}">` + SubjectData[request.subjectCode[i]] +`<div class="ui red label">${request.AbsentList[i].length}</div></a>`;
-        }
-        document.getElementById("subMenu").innerHTML = menuContent;
-        $('.item').popup();
-        document.getElementById("subMenuValue").innerHTML = request.AbsentList[0];
-        let buttonList = document.getElementById("subMenu").getElementsByTagName('a');
-        for(let i=0;i<buttonList.length;i++)
-        {
-            buttonList[i].onclick = function() {
-                let tempList = document.getElementById("subMenu").getElementsByTagName('a');
-                for(let j=0;j<tempList.length;j++)
-                {
-                    if (tempList[j].classList.contains("active")) {
-                        tempList[j].classList.remove("active");
-                    }
-                }
-                this.classList.add("active");
-                document.getElementById("subMenuValue").innerHTML = request.AbsentList[request.subjectCode.indexOf(this.id)];
-            }
-        }
+        HideLoading();
+        attendanceListAppend(request);
     });
 
     let attendance = document.getElementById("attendance")
@@ -39,6 +18,7 @@ window.onload = () => {
     let subjectLink = null;
     attendance.onclick = () => {
         console.log("Attendance Click");
+        AddLoading();
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { msg: 0 }, function (response) {
                 console.log(response.farewell);
@@ -56,8 +36,59 @@ window.onload = () => {
     }
 
     document.getElementById('github').onclick = () => {
-        console.log("github");
+        console.log("Gthub Click");
         let newURL = "https://github.com/abhishekr700/auto-ims-captcha";
         chrome.tabs.create({ url: newURL });
+    }
+}
+
+let AddLoading = () => {
+    if (document.getElementById("loading").classList.contains("hidden")) {
+        document.getElementById("loading").classList.remove("hidden");
+    }
+}
+
+let HideLoading = () => {
+    if (!document.getElementById("loading").classList.contains("hidden")) {
+        document.getElementById("loading").classList.add("hidden");
+    }
+}
+
+let attendanceListAppend = (request) => {
+    let menu = `<div class="eight wide column">
+                        <div class="ui large vertical menu" id="subMenu">
+                            <!-- FILLED THROUGH JAVASCRIPT -->
+                        </div>
+                    </div>
+                    <div class="eight wide stretched column">
+                        <div class="ui segment">
+                            <p id="subMenuValue">
+                                <!-- FILLED THROUGH JAVASCRIPT -->
+                            </p>
+                        </div>
+                    </div>`
+    document.getElementById('container').innerHTML = menu;
+    let menuContent = `<a id = ${request.subjectCode[0]} class="active item" data-content="${request.subjectCode[0]}">` + SubjectData[request.subjectCode[0]] + `<div class="ui red label">${request.AbsentList[0].length}</div></a>`;
+    for(let i=1;i<request.subjectCode.length;i++)
+    {
+        menuContent += `<a id = ${request.subjectCode[i]} class="item" data-content="${request.subjectCode[i]}">` + SubjectData[request.subjectCode[i]] +`<div class="ui red label">${request.AbsentList[i].length}</div></a>`;
+    }
+    document.getElementById("subMenu").innerHTML = menuContent;
+    $('.item').popup();
+    document.getElementById("subMenuValue").innerHTML = request.AbsentList[0];
+    let buttonList = document.getElementById("subMenu").getElementsByTagName('a');
+    for(let i=0;i<buttonList.length;i++)
+    {
+        buttonList[i].onclick = function() {
+            let tempList = document.getElementById("subMenu").getElementsByTagName('a');
+            for(let j=0;j<tempList.length;j++)
+            {
+                if (tempList[j].classList.contains("active")) {
+                    tempList[j].classList.remove("active");
+                }
+            }
+            this.classList.add("active");
+            document.getElementById("subMenuValue").innerHTML = request.AbsentList[request.subjectCode.indexOf(this.id)];
+        }
     }
 }
